5f7a0b61af88c52fb57a3690092a5f18d87deacc
Mark Sailes <msailes@amazon.co.uk>
2020-05-18 10:08:39 +0100

    Migrating to use Lombok.

--- aws-lambda-java-events/src/main/java/com/amazonaws/services/lambda/runtime/events/SecretsManagerRotationEvent.java
+++ aws-lambda-java-events/src/main/java/com/amazonaws/services/lambda/runtime/events/SecretsManagerRotationEvent.java
@@ -13,6 +13,9 @@
 
 package com.amazonaws.services.lambda.runtime.events;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Class to represent the events which are sent during a Secrets Manager rotation process.
  *
@@ -20,109 +23,13 @@ package com.amazonaws.services.lambda.runtime.events;
  *
  * @author msailes <msailes@amazon.co.uk>
  */
+
+@NoArgsConstructor
+@Data
 public class SecretsManagerRotationEvent {
 
     private String step;
     private String secretId;
     private String clientRequestToken;
 
-    /**
-     * Default constructor
-     */
-    public SecretsManagerRotationEvent() {
-    }
-
-    /**
-     * The current step in the rotation process.
-     *
-     * @return step
-     */
-    public String getStep() {
-        return step;
-    }
-
-    /**
-     * Sets the current step in the rotation process.
-     *
-     * @param step
-     */
-    public void setStep(String step) {
-        this.step = step;
-    }
-
-    /**
-     * The ID or Amazon Resource Name (ARN) for the secret to rotate.
-     *
-     * @return secretId
-     */
-    public String getSecretId() {
-        return secretId;
-    }
-
-    /**
-     * Sets the ID or ARN for the secret to rotate.
-     *
-     * @param secretId
-     */
-    public void setSecretId(String secretId) {
-        this.secretId = secretId;
-    }
-
-    /**
-     * The request token Secrets Manager uses this token to ensure the idempotency of requests during any required
-     * retries caused by failures of individual calls.
-     *
-     * @return clientRequestToken
-     */
-    public String getClientRequestToken() {
-        return clientRequestToken;
-    }
-
-    /**
-     * Sets the request token.
-     *
-     * @param clientRequestToken
-     */
-    public void setClientRequestToken(String clientRequestToken) {
-        this.clientRequestToken = clientRequestToken;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        SecretsManagerRotationEvent that = (SecretsManagerRotationEvent) o;
-
-        if (step != null ? !step.equals(that.step) : that.step != null) return false;
-        if (secretId != null ? !secretId.equals(that.secretId) : that.secretId != null) return false;
-        return clientRequestToken != null ? clientRequestToken.equals(that.clientRequestToken) : that.clientRequestToken == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = step != null ? step.hashCode() : 0;
-        result = 31 * result + (secretId != null ? secretId.hashCode() : 0);
-        result = 31 * result + (clientRequestToken != null ? clientRequestToken.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("SecretsManagerRotationEvent{");
-        sb.append("step='").append(step).append('\'');
-        sb.append(", secretId='").append(secretId).append('\'');
-        sb.append(", clientRequestToken='").append(clientRequestToken).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
-
-    @Override
-    public SecretsManagerRotationEvent clone() {
-        try {
-            return (SecretsManagerRotationEvent) super.clone();
-        } catch (CloneNotSupportedException e) {
-            throw new IllegalStateException("Got a CloneNotSupportedException from Object.clone()", e);
-        }
-    }
 }

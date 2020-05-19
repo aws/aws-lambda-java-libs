d37b5feeb04f29127bdbe576a3f8b7a514ba0c05
Mark Sailes <45629314+msailes@users.noreply.github.com>
2020-05-19 11:40:36 +0100

    Add Application Load Balancer Target Events (#131)
    * Classes to represent the request and response events for Application Load Balancer integration.

---
+++ aws-lambda-java-events/src/main/java/com/amazonaws/services/lambda/runtime/events/ApplicationLoadBalancerRequestEvent.java
@@ -0,0 +1,47 @@
+package com.amazonaws.services.lambda.runtime.events;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class to represent the request event from Application Load Balancer.
+ *
+ * @see <a href="https://docs.aws.amazon.com/lambda/latest/dg/services-alb.html">Using AWS Lambda with an Application Load Balancer</a>
+ *
+ * @author msailes <msailes@amazon.co.uk>
+ */
+
+@NoArgsConstructor
+@Data
+public class ApplicationLoadBalancerRequestEvent implements Serializable, Cloneable  {
+
+    @NoArgsConstructor
+    @Data
+    public static class Elb implements Serializable, Cloneable {
+
+        private String targetGroupArn;
+
+    }
+
+    @NoArgsConstructor
+    @Data
+    public static class RequestContext implements Serializable, Cloneable {
+
+        private Elb elb;
+
+    }
+
+    private RequestContext requestContext;
+    private String httpMethod;
+    private String path;
+    private Map<String, String> queryStringParameters;
+    private Map<String, String> headers;
+    private Map<String, List<String>> multiValueHeaders;
+    private String body;
+    private boolean isBase64Encoded;
+
+}

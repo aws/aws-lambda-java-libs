956c4fa279a1dc18eacd35c66d50577278a4efad
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2020-06-08 10:10:29 -0700

    fix npe when mapping insert / delete events.
    Insert event does not have oldImage. Remove event does not have newImage. At this moment library just throws nullPointerException

--- aws-lambda-java-events-sdk-transformer/src/main/java/com/amazonaws/services/lambda/runtime/events/transformers/dynamodb/DynamodbStreamRecordTransformer.java
+++ aws-lambda-java-events-sdk-transformer/src/main/java/com/amazonaws/services/lambda/runtime/events/transformers/dynamodb/DynamodbStreamRecordTransformer.java
@@ -1,10 +1,20 @@
 package com.amazonaws.services.lambda.runtime.events.transformers.dynamodb;
 
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.dynamodb.model.StreamRecord;
+
+import java.util.Map;
 
 public class DynamodbStreamRecordTransformer {
 
     public static StreamRecord toStreamRecordV2(final com.amazonaws.services.lambda.runtime.events.models.dynamodb.StreamRecord streamRecord) {
+        Map<String, AttributeValue> newImage = streamRecord.getNewImage() != null
+                ? DynamodbAttributeValueTransformer.toAttributeValueMapV2(streamRecord.getNewImage())
+                : null;
+        Map<String, AttributeValue> oldImage = streamRecord.getOldImage() != null
+                ? DynamodbAttributeValueTransformer.toAttributeValueMapV2(streamRecord.getOldImage())
+                : null;
+
         return StreamRecord.builder()
                 .approximateCreationDateTime(
                         streamRecord.getApproximateCreationDateTime().toInstant()
@@ -12,12 +22,8 @@ public class DynamodbStreamRecordTransformer {
                 .keys(
                         DynamodbAttributeValueTransformer.toAttributeValueMapV2(streamRecord.getKeys())
                 )
-                .newImage(
-                        DynamodbAttributeValueTransformer.toAttributeValueMapV2(streamRecord.getNewImage())
-                )
-                .oldImage(
-                        DynamodbAttributeValueTransformer.toAttributeValueMapV2(streamRecord.getOldImage())
-                )
+                .newImage(newImage)
+                .oldImage(oldImage)
                 .sequenceNumber(streamRecord.getSequenceNumber())
                 .sizeBytes(streamRecord.getSizeBytes())
                 .streamViewType(streamRecord.getStreamViewType())

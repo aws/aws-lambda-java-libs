d262d0f55e85b3934437d9ba279cc3ee0af0cee9
prajeshr <prajesh1@users.noreply.github.com>
2020-08-11 09:35:46 -0700

    Add Kafka Event (#154)
    Co-authored-by: Prajesh Ravindran <prajeshr@amazon.com>

---
+++ aws-lambda-java-events/src/main/java/com/amazonaws/services/lambda/runtime/events/KafkaEvent.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2015-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with
+ * the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.amazonaws.services.lambda.runtime.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(setterPrefix = "with")
+/** Represents a Kafka Event. **/
+public class KafkaEvent {
+    private Map<String, List<KafkaEventRecord>> records;
+    private  String eventSource;
+    private String eventSourceArn;
+    
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder(setterPrefix = "with")
+    public static class KafkaEventRecord {
+        private String topic;
+        private int partition;
+        private long offset;
+        private long timestamp;
+        private String timestampType;
+        private String key;
+        private String value;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder(setterPrefix = "with")
+    public static class TopicPartition {
+        private  String topic;
+        private  int partition;
+
+        @Override
+        public String toString() {
+            //Kafka also uses '-' for toString()
+            return topic + "-" + partition;
+        }
+    }
+}

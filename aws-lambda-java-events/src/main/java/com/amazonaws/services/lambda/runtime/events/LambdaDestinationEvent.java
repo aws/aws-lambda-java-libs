653dbcb44d4ff343533eec1885d927d30927cade
Mark Sailes <45629314+msailes@users.noreply.github.com>
2020-09-14 14:56:25 +0100

    Add support for Lambda Destination Event (#139)

---
+++ aws-lambda-java-events/src/main/java/com/amazonaws/services/lambda/runtime/events/LambdaDestinationEvent.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with
+ * the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.amazonaws.services.lambda.runtime.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.joda.time.DateTime;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * Class to represent an invocation record for a Lambda event.
+ *
+ * @see <a href="https://docs.aws.amazon.com/lambda/latest/dg/invocation-async.html>Asynchronous invocation</a>
+ *
+ * @author msailes <msailes@amazon.co.uk>
+ */
+
+@Data
+@Builder(setterPrefix = "with")
+@NoArgsConstructor
+@AllArgsConstructor
+public class LambdaDestinationEvent implements Serializable, Cloneable {
+
+    private String version;
+    private DateTime timestamp;
+    private RequestContext requestContext;
+    private Map<String, Object> requestPayload;
+    private Object responseContext;
+    private Object responsePayload;
+
+    @Data
+    @Builder(setterPrefix = "with")
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class RequestContext implements Serializable, Cloneable {
+        private String requestId;
+        private String functionArn;
+        private String condition;
+        private int approximateInvokeCount;
+    }
+}

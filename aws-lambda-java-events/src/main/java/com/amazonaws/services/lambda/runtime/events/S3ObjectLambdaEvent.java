ec238959abd675cedb0749f6e579e441da5c7ef1
msailes <msailes@amazon.co.uk>
2021-03-23 13:29:19 +0000

    Helpers for the most used methods. Improved javadocs. Example usage added.

--- aws-lambda-java-events/src/main/java/com/amazonaws/services/lambda/runtime/events/S3ObjectLambdaEvent.java
+++ aws-lambda-java-events/src/main/java/com/amazonaws/services/lambda/runtime/events/S3ObjectLambdaEvent.java
@@ -10,7 +10,41 @@ import java.util.Map;
 /**
  * Event to allow transformations to occur before an S3 object is returned to the calling service.
  *
- * https://aws.amazon.com/blogs/aws/introducing-amazon-s3-object-lambda-use-your-code-to-process-data-as-it-is-being-retrieved-from-s3/
+ * <strong>Documentation</strong>
+ *
+ * <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/olap-writing-lambda.html">Writing and debugging Lambda functions for S3 Object Lambda Access Points</a>
+ *
+ * <strong>Example:</strong>
+ *
+ * <pre>
+ * <code>import com.amazonaws.services.lambda.runtime.Context;
+ * import com.amazonaws.services.lambda.runtime.events.S3ObjectLambdaEvent;
+ * import org.apache.http.client.fluent.Request;
+ * import software.amazon.awssdk.services.s3.S3Client;
+ * import software.amazon.awssdk.services.s3.model.WriteGetObjectResponseRequest;
+ *
+ * import java.io.IOException;
+ *
+ * import static software.amazon.awssdk.core.sync.RequestBody.fromString;
+ *
+ * public class S3ObjectRequestHandler {
+ *
+ *      private static final S3Client s3Client = S3Client.create();
+ *
+ *      public void handleRequest(S3ObjectLambdaEvent event, Context context) throws IOException {
+ *          String s3Body = Request.Get(event.inputS3Url()).execute().returnContent().asString();
+ *
+ *          String responseBody = s3Body.toUpperCase();
+ *
+ *          WriteGetObjectResponseRequest request = WriteGetObjectResponseRequest.builder()
+ *              .requestRoute(event.outputRoute())
+ *              .requestToken(event.outputToken())
+ *              .build();
+ *          s3Client.writeGetObjectResponse(request, fromString(responseBody));
+ *      }
+ * }
+ * </code>
+ * </pre>
  */
 
 @Data
@@ -25,6 +59,39 @@ public class S3ObjectLambdaEvent {
     private UserRequest userRequest;
     private UserIdentity userIdentity;
     private String protocolVersion;
+
+    /**
+     * A pre-signed URL that can be used to fetch the original object from Amazon S3.
+     *
+     * The URL is signed using the original caller’s identity, and their permissions
+     * will apply when the URL is used. If there are signed headers in the URL, the
+     * Lambda function must include these in the call to Amazon S3, except for the Host.
+     *
+     * @return A pre-signed URL that can be used to fetch the original object from Amazon S3.
+     */
+    public String inputS3Url() {
+        return getGetObjectContext().getInputS3Url();
+    }
+
+    /**
+     * A routing token that is added to the S3 Object Lambda URL when the Lambda function
+     * calls the S3 API WriteGetObjectResponse.
+     *
+     * @return the outputRoute
+     */
+    public String outputRoute() {
+        return getGetObjectContext().getOutputRoute();
+    }
+
+    /**
+     * An opaque token used by S3 Object Lambda to match the WriteGetObjectResponse call
+     * with the original caller.
+     *
+     * @return the outputToken
+     */
+    public String outputToken() {
+        return getGetObjectContext().getOutputToken();
+    }
 
     @Data
     @Builder(setterPrefix = "with")

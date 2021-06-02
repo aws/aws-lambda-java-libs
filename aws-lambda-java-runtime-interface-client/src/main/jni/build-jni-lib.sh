4c59dbfb46976bf9443f29daf2052143006b018c
preearor <69850117+preearor@users.noreply.github.com>
2021-06-02 15:47:04 +0100

    Update Curl version to 7.77.0, change libcurl distribution to tarball (#249)
    Co-authored-by: Carl Zogheib <11421173+carlzogh@users.noreply.github.com>

--- aws-lambda-java-runtime-interface-client/src/main/jni/build-jni-lib.sh
+++ aws-lambda-java-runtime-interface-client/src/main/jni/build-jni-lib.sh
@@ -5,11 +5,12 @@ set -euo pipefail
 
 SRC_DIR=$(dirname "$0")
 DST_DIR=${1}
+CURL_VERSION=7.77.0
 
 # compile the native library
-docker build -f "${SRC_DIR}/Dockerfile.glibc" -t lambda-java-jni-lib-glibc "${SRC_DIR}"
+docker build -f "${SRC_DIR}/Dockerfile.glibc" --build-arg CURL_VERSION=${CURL_VERSION} -t lambda-java-jni-lib-glibc "${SRC_DIR}"
 docker run --rm --entrypoint /bin/cat lambda-java-jni-lib-glibc /src/aws-lambda-runtime-interface-client.so > "${DST_DIR}"/classes/aws-lambda-runtime-interface-client.glibc.so
 
-docker build -f "${SRC_DIR}/Dockerfile.musl" -t lambda-java-jni-lib-musl "${SRC_DIR}"
+docker build -f "${SRC_DIR}/Dockerfile.musl" --build-arg CURL_VERSION=${CURL_VERSION} -t lambda-java-jni-lib-musl "${SRC_DIR}"
 docker run --rm --entrypoint /bin/cat lambda-java-jni-lib-musl /src/aws-lambda-runtime-interface-client.so > "${DST_DIR}"/classes/aws-lambda-runtime-interface-client.musl.so
 

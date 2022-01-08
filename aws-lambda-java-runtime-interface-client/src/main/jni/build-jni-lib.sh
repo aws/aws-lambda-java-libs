c0197c60667ffd804cf14135ffaa05c3c169d1f1
Christian Mueller <cmr@amazon.de>
2022-01-08 12:44:23 +0100

    fixed build-jni-lib.sh script after refactoring it

--- aws-lambda-java-runtime-interface-client/src/main/jni/build-jni-lib.sh
+++ aws-lambda-java-runtime-interface-client/src/main/jni/build-jni-lib.sh
@@ -7,11 +7,14 @@ SRC_DIR=$(dirname "$0")
 DST_DIR=${1}
 CURL_VERSION=7.77.0
 
-for arch in x86 arm; do
+for i in x86,linux/amd64 arm,linux/arm64/v8; do
+  arch=${i%,*};
+  platform=${i#*,};
+
   mkdir "${DST_DIR}"/classes/$arch
-  docker build --platform=linux/amd64 -f "${SRC_DIR}/Dockerfile.glibc" --build-arg CURL_VERSION=${CURL_VERSION} -t lambda-java-jni-lib-glibc-$arch "${SRC_DIR}"
+  docker build --platform=$platform -f "${SRC_DIR}/Dockerfile.glibc" --build-arg CURL_VERSION=${CURL_VERSION} -t lambda-java-jni-lib-glibc-$arch "${SRC_DIR}"
   docker run --rm --entrypoint /bin/cat lambda-java-jni-lib-glibc-$arch /src/aws-lambda-runtime-interface-client.so > "${DST_DIR}"/classes/$arch/aws-lambda-runtime-interface-client.glibc.so
 
-  docker build --platform=linux/amd64 -f "${SRC_DIR}/Dockerfile.musl" --build-arg CURL_VERSION=${CURL_VERSION} -t lambda-java-jni-lib-musl-$arch "${SRC_DIR}"
+  docker build --platform=$platform -f "${SRC_DIR}/Dockerfile.musl" --build-arg CURL_VERSION=${CURL_VERSION} -t lambda-java-jni-lib-musl-$arch "${SRC_DIR}"
   docker run --rm --entrypoint /bin/cat lambda-java-jni-lib-musl-$arch /src/aws-lambda-runtime-interface-client.so > "${DST_DIR}"/classes/$arch/aws-lambda-runtime-interface-client.musl.so
 done

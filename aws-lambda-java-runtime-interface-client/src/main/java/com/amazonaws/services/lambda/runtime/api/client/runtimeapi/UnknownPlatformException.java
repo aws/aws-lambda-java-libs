9bc66d9262195afe148324117986b61a9cf3f3f1
Carl Zogheib <carlzogh@amazon.com>
2022-01-17 11:52:53 +0000

    Update UnknownPlatformException.java

--- aws-lambda-java-runtime-interface-client/src/main/java/com/amazonaws/services/lambda/runtime/api/client/runtimeapi/UnknownPlatformException.java
+++ aws-lambda-java-runtime-interface-client/src/main/java/com/amazonaws/services/lambda/runtime/api/client/runtimeapi/UnknownPlatformException.java
@@ -1,7 +1,7 @@
 package com.amazonaws.services.lambda.runtime.api.client.runtimeapi;
 
 /**
- * Copyright (c) 2019 Amazon. All rights reserved.
+ * Copyright (c) 2022 Amazon. All rights reserved.
  */
 public class UnknownPlatformException extends RuntimeException {
 

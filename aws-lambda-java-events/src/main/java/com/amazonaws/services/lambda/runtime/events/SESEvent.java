417bed5b0d61c102c5cc21cc10da0b0a16d9728c
Jerome Van Der Linden <jeromevdl@gmail.com>
2022-06-02 15:14:41 +0200

    add SES event

--- aws-lambda-java-events/src/main/java/com/amazonaws/services/lambda/runtime/events/SESEvent.java
+++ aws-lambda-java-events/src/main/java/com/amazonaws/services/lambda/runtime/events/SESEvent.java
@@ -1,13 +1,14 @@
 package com.amazonaws.services.lambda.runtime.events;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.joda.time.DateTime;
 
+import java.io.Serializable;
+import java.time.Instant;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Represent an event received from SES when it receives an incoming message.
@@ -18,62 +19,146 @@ import java.util.Map;
 @AllArgsConstructor
 @Builder(setterPrefix = "with")
 @NoArgsConstructor
-public class SESEvent {
+public class SESEvent implements Serializable, Cloneable {
 
-    private List<Record> Records;
+    private static final long serialVersionUID = 4378251841610238092L;
+    
+    @JsonProperty("Records")
+    private List<Record> records;
+
+    @Override
+    public SESEvent clone() {
+        try {
+            return (SESEvent) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new IllegalStateException("Got a CloneNotSupportedException from Object.clone()", e);
+        }
+    }
 
     @Data
     @AllArgsConstructor
     @Builder(setterPrefix = "with")
     @NoArgsConstructor
-    private static class Record {
+    public static class Record implements Serializable, Cloneable {
+
+        private static final long serialVersionUID = 4689200289017967041L;
+
         private String eventSource;
         private String eventVersion;
         private Ses ses;
+
+        @Override
+        public Record clone() {
+            try {
+                return (Record) super.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new IllegalStateException("Got a CloneNotSupportedException from Object.clone()", e);
+            }
+        }
     }
 
     @Data
     @AllArgsConstructor
     @Builder(setterPrefix = "with")
     @NoArgsConstructor
-    private static class Ses {
+    public static class Ses implements Serializable, Cloneable {
+
+        private static final long serialVersionUID = -8671051823119970975L;
+
         private Mail mail;
         private Receipt receipt;
+
+        @Override
+        public Ses clone() {
+            try {
+                return (Ses) super.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new IllegalStateException("Got a CloneNotSupportedException from Object.clone()", e);
+            }
+        }
     }
 
     @Data
     @AllArgsConstructor
     @Builder(setterPrefix = "with")
     @NoArgsConstructor
-    private static class Mail {
-        private DateTime timestamp;
+    public static class Mail implements Serializable, Cloneable {
+
+        private static final long serialVersionUID = 4055476288790904705L;
+
+        private Instant timestamp;
         private String source;
         private String messageId;
         private String[] destination;
         private boolean headersTruncated;
-        private Map<String, String> headers;
+        private List<Header> headers;
         private CommonHeaders commonHeaders;
+
+        @Override
+        public Mail clone() {
+            try {
+                return (Mail) super.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new IllegalStateException("Got a CloneNotSupportedException from Object.clone()", e);
+            }
+        }
     }
 
     @Data
     @AllArgsConstructor
     @Builder(setterPrefix = "with")
     @NoArgsConstructor
-    private static class CommonHeaders {
+    public static class Header implements Serializable, Cloneable {
+
+        private static final long serialVersionUID = 1059624012149639930L;
+
+        private String name;
+        private String value;
+
+        @Override
+        public Header clone() {
+            try {
+                return (Header) super.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new IllegalStateException("Got a CloneNotSupportedException from Object.clone()", e);
+            }
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    @Builder(setterPrefix = "with")
+    @NoArgsConstructor
+    public static class CommonHeaders implements Serializable, Cloneable {
+
+        private static final long serialVersionUID = 4926097072068505243L;
+
         private String returnPath;
         private String[] from;
         private String date;
         private String[] to;
         private String messageId;
         private String subject;
+
+        @Override
+        public CommonHeaders clone() {
+            try {
+                return (CommonHeaders) super.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new IllegalStateException("Got a CloneNotSupportedException from Object.clone()", e);
+            }
+        }
     }
 
     @Data
     @AllArgsConstructor
     @Builder(setterPrefix = "with")
     @NoArgsConstructor
-    private static class Receipt {
-        private DateTime timestamp;
+    public static class Receipt implements Serializable, Cloneable {
+
+        private static final long serialVersionUID = -2378481926296810065L;
+
+        private Instant timestamp;
         private long processingTimeMillis;
         private String[] recipients;
         private Action action;
@@ -82,23 +167,56 @@ public class SESEvent {
         private Verdict spfVerdict;
         private Verdict dkimVerdict;
         private Verdict dmarcVerdict;
+
+        @Override
+        public Receipt clone() {
+            try {
+                return (Receipt) super.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new IllegalStateException("Got a CloneNotSupportedException from Object.clone()", e);
+            }
+        }
     }
 
     @Data
     @AllArgsConstructor
     @Builder(setterPrefix = "with")
     @NoArgsConstructor
-    private static class Action {
+    public static class Action implements Serializable, Cloneable {
+
+        private static final long serialVersionUID = 292227308414074012L;
+
         private String type;
         private String functionArn;
         private String invocationType;
+
+        @Override
+        public Action clone() {
+            try {
+                return (Action) super.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new IllegalStateException("Got a CloneNotSupportedException from Object.clone()", e);
+            }
+        }
     }
 
     @Data
     @AllArgsConstructor
     @Builder(setterPrefix = "with")
     @NoArgsConstructor
-    private static class Verdict {
+    public static class Verdict implements Serializable, Cloneable {
+
+        private static final long serialVersionUID = 773331220324957901L;
+
         private String status;
+
+        @Override
+        public Verdict clone() {
+            try {
+                return (Verdict) super.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new IllegalStateException("Got a CloneNotSupportedException from Object.clone()", e);
+            }
+        }
     }
 }

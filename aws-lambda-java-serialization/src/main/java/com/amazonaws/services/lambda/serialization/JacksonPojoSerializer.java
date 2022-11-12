04347057603343d559aea21c5efc3c8f6666e8a9
Andrea Culot <andclt@amazon.com>
2022-11-12 13:22:59 +0000

    Bump jackson-databind and gson in /aws-lambda-java-serialization

--- aws-lambda-java-serialization/src/main/java/com/amazonaws/services/lambda/serialization/JacksonPojoSerializer.java
+++ aws-lambda-java-serialization/src/main/java/com/amazonaws/services/lambda/serialization/JacksonPojoSerializer.java
@@ -16,6 +16,7 @@ import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationConfig;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -57,23 +58,41 @@ public class JacksonPojoSerializer implements CustomPojoSerializer {
 
 
     private static ObjectMapper createObjectMapper() {
-        ObjectMapper mapper = new ObjectMapper(createJsonFactory());
+        ObjectMapper mapper = JsonMapper.builder(createJsonFactory())
+                .enable(MapperFeature.ALLOW_FINAL_FIELDS_AS_MUTATORS)		                // this is default as of 2.13.4.2
+                .enable(MapperFeature.AUTO_DETECT_FIELDS)			                        // this is default as of 2.13.4.2
+                .enable(MapperFeature.AUTO_DETECT_GETTERS)		                            // this is default as of 2.13.4.2
+                .enable(MapperFeature.AUTO_DETECT_IS_GETTERS)			                    // this is default as of 2.13.4.2
+                .enable(MapperFeature.AUTO_DETECT_SETTERS)			                        // this is default as of 2.13.4.2
+                .enable(MapperFeature.CAN_OVERRIDE_ACCESS_MODIFIERS)		                // this is default as of 2.13.4.2
+                .enable(MapperFeature.USE_STD_BEAN_NAMING)
+                .enable(MapperFeature.USE_ANNOTATIONS)				                        // this is default as of 2.13.4.2
+                .disable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)	                // this is default as of 2.13.4.2
+                .disable(MapperFeature.AUTO_DETECT_CREATORS)
+                .disable(MapperFeature.INFER_PROPERTY_MUTATORS)
+                .disable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY)	                    // this is default as of 2.13.4.2
+                .disable(MapperFeature.USE_GETTERS_AS_SETTERS)
+                .disable(MapperFeature.USE_WRAPPER_NAME_AS_PROPERTY_NAME)	                // this is default as of 2.13.4.2
+                .disable(MapperFeature.USE_STATIC_TYPING)		                            // this is default as of 2.13.4.2
+                .disable(MapperFeature.REQUIRE_SETTERS_FOR_GETTERS)		                    // this is default as of 2.13.4.2
+                .build();
+
         SerializationConfig scfg = mapper.getSerializationConfig();
         scfg = scfg.withFeatures(
-                SerializationFeature.FAIL_ON_SELF_REFERENCES,			    //this is default as of 2.12.3
-                SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS,	//this is default as of 2.12.3
-                SerializationFeature.WRAP_EXCEPTIONS				        //this is default as of 2.12.3
+                SerializationFeature.FAIL_ON_SELF_REFERENCES,			                    // this is default as of 2.13.4.2
+                SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS,	                // this is default as of 2.13.4.2
+                SerializationFeature.WRAP_EXCEPTIONS				                        // this is default as of 2.13.4.2
         );
         scfg = scfg.withoutFeatures(
-                SerializationFeature.CLOSE_CLOSEABLE,				        //this is default as of 2.12.3
+                SerializationFeature.CLOSE_CLOSEABLE,				                        // this is default as of 2.13.4.2
                 SerializationFeature.EAGER_SERIALIZER_FETCH,
                 SerializationFeature.FAIL_ON_EMPTY_BEANS,
                 SerializationFeature.FLUSH_AFTER_WRITE_VALUE,
-                SerializationFeature.INDENT_OUTPUT,				            //this is default as of 2.12.3
-                SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS,			    //this is default as of 2.12.3
-                SerializationFeature.USE_EQUALITY_FOR_OBJECT_ID,		    //this is default as of 2.12.3
-                SerializationFeature.WRITE_CHAR_ARRAYS_AS_JSON_ARRAYS,		//this is default as of 2.12.3
-                SerializationFeature.WRAP_ROOT_VALUE				        //this is default as of 2.12.3
+                SerializationFeature.INDENT_OUTPUT,				                            // this is default as of 2.13.4.2
+                SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS,			                    // this is default as of 2.13.4.2
+                SerializationFeature.USE_EQUALITY_FOR_OBJECT_ID,		                    // this is default as of 2.13.4.2
+                SerializationFeature.WRITE_CHAR_ARRAYS_AS_JSON_ARRAYS,		                // this is default as of 2.13.4.2
+                SerializationFeature.WRAP_ROOT_VALUE				                        // this is default as of 2.13.4.2
         );
         mapper.setConfig(scfg);
 
@@ -82,39 +101,21 @@ public class JacksonPojoSerializer implements CustomPojoSerializer {
                 DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT,
                 DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT,
                 DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY,
-                DeserializationFeature.FAIL_ON_INVALID_SUBTYPE,			    //this is default as of 2.12.3
-                DeserializationFeature.FAIL_ON_UNRESOLVED_OBJECT_IDS,		//this is default as of 2.12.3
+                DeserializationFeature.FAIL_ON_INVALID_SUBTYPE,			                    // this is default as of 2.13.4.2
+                DeserializationFeature.FAIL_ON_UNRESOLVED_OBJECT_IDS,		                // this is default as of 2.13.4.2
                 DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL,
                 DeserializationFeature.UNWRAP_SINGLE_VALUE_ARRAYS,
-                DeserializationFeature.WRAP_EXCEPTIONS				        //this is default as of 2.12.3
+                DeserializationFeature.WRAP_EXCEPTIONS				                        // this is default as of 2.13.4.2
         );
         dcfg = dcfg.withoutFeatures(
-                DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES,		    //this is default as of 2.12.3
-                DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES,		    //this is default as of 2.12.3
-                DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS,		    //this is default as of 2.12.3
-                DeserializationFeature.FAIL_ON_READING_DUP_TREE_KEY,		//this is default as of 2.12.3
+                DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES,		                    // this is default as of 2.13.4.2
+                DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES,		                    // this is default as of 2.13.4.2
+                DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS,		                    // this is default as of 2.13.4.2
+                DeserializationFeature.FAIL_ON_READING_DUP_TREE_KEY,		                // this is default as of 2.13.4.2
                 DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
         );
         mapper.setConfig(dcfg);
-        mapper.setSerializationInclusion(Include.NON_NULL);                 //NON_EMPTY?
-
-        mapper.enable(MapperFeature.ALLOW_FINAL_FIELDS_AS_MUTATORS);		//this is default as of 2.12.3
-        mapper.enable(MapperFeature.AUTO_DETECT_FIELDS);			        //this is default as of 2.12.3
-        mapper.enable(MapperFeature.AUTO_DETECT_GETTERS);			        //this is default as of 2.12.3
-        mapper.enable(MapperFeature.AUTO_DETECT_IS_GETTERS);			    //this is default as of 2.12.3
-        mapper.enable(MapperFeature.AUTO_DETECT_SETTERS);			        //this is default as of 2.12.3
-        mapper.enable(MapperFeature.CAN_OVERRIDE_ACCESS_MODIFIERS);		    //this is default as of 2.12.3
-        mapper.enable(MapperFeature.USE_STD_BEAN_NAMING);
-        mapper.enable(MapperFeature.USE_ANNOTATIONS);				        //this is default as of 2.12.3
-
-        mapper.disable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES);	//this is default as of 2.12.3
-        mapper.disable(MapperFeature.AUTO_DETECT_CREATORS);
-        mapper.disable(MapperFeature.INFER_PROPERTY_MUTATORS);
-        mapper.disable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);		//this is default as of 2.12.3
-        mapper.disable(MapperFeature.USE_GETTERS_AS_SETTERS);
-        mapper.disable(MapperFeature.USE_WRAPPER_NAME_AS_PROPERTY_NAME);	//this is default as of 2.12.3
-        mapper.disable(MapperFeature.USE_STATIC_TYPING);			        //this is default as of 2.12.3
-        mapper.disable(MapperFeature.REQUIRE_SETTERS_FOR_GETTERS);		    //this is default as of 2.12.3
+        mapper.setSerializationInclusion(Include.NON_NULL);
 
         SimpleModule module = new SimpleModule();
         module.addDeserializer(Void.class, new VoidDeserializer());
@@ -158,16 +159,16 @@ public class JacksonPojoSerializer implements CustomPojoSerializer {
                 .enable(JsonReadFeature.ALLOW_UNQUOTED_FIELD_NAMES)
 
                 //Json Read disabled
-                .disable(JsonReadFeature.ALLOW_JAVA_COMMENTS)			            //this is default as of 2.12.3
-                .disable(JsonReadFeature.ALLOW_YAML_COMMENTS)			            //this is default as of 2.12.3
+                .disable(JsonReadFeature.ALLOW_JAVA_COMMENTS)			            // this is default as of 2.13.4.2
+                .disable(JsonReadFeature.ALLOW_YAML_COMMENTS)			            // this is default as of 2.13.4.2
 
                 //Json Write enabled
-                .enable(JsonWriteFeature.QUOTE_FIELD_NAMES)                         //this is default as of 2.12.3
-                .enable(JsonWriteFeature.WRITE_NAN_AS_STRINGS)                      //this is default as of 2.12.3
+                .enable(JsonWriteFeature.QUOTE_FIELD_NAMES)                         // this is default as of 2.13.4.2
+                .enable(JsonWriteFeature.WRITE_NAN_AS_STRINGS)                      // this is default as of 2.13.4.2
 
                 //Json Write disabled
-                .disable(JsonWriteFeature.ESCAPE_NON_ASCII)                         //this is default as of 2.12.3
-                .disable(JsonWriteFeature.WRITE_NUMBERS_AS_STRINGS)                 //this is default as of 2.12.3
+                .disable(JsonWriteFeature.ESCAPE_NON_ASCII)                         // this is default as of 2.13.4.2
+                .disable(JsonWriteFeature.WRITE_NUMBERS_AS_STRINGS)                 // this is default as of 2.13.4.2
                 .build();
 
         //Json Parser disabled
@@ -181,8 +182,8 @@ public class JacksonPojoSerializer implements CustomPojoSerializer {
         factory.disable(JsonGenerator.Feature.AUTO_CLOSE_JSON_CONTENT);
         factory.disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
         factory.disable(JsonGenerator.Feature.FLUSH_PASSED_TO_STREAM);
-        factory.disable(JsonGenerator.Feature.STRICT_DUPLICATE_DETECTION);	    //this is default as of 2.12.3
-        factory.disable(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN);       //this is default as of 2.12.3
+        factory.disable(JsonGenerator.Feature.STRICT_DUPLICATE_DETECTION);	        // this is default as of 2.13.4.2
+        factory.disable(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN);           // this is default as of 2.13.4.2
 
         return factory;
     }
@@ -190,7 +191,7 @@ public class JacksonPojoSerializer implements CustomPojoSerializer {
     @Override
     public <T> T fromJson(InputStream input, Type type) {
         try {
-            return mapper.reader(mapper.constructType(type)).readValue(input);
+            return mapper.readerFor(mapper.constructType(type)).readValue(input);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
@@ -199,7 +200,7 @@ public class JacksonPojoSerializer implements CustomPojoSerializer {
     @Override
     public <T> T fromJson(String input, Type type) {
         try {
-            return mapper.reader(mapper.constructType(type)).readValue(input);
+            return mapper.readerFor(mapper.constructType(type)).readValue(input);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }

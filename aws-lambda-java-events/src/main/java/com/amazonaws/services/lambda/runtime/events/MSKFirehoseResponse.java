6fa478eda52b51b4437a9acb086a1b71bf93cc02
Shashank <48707265+ShashankAWS@users.noreply.github.com>
2024-07-04 03:03:30 +0530

    Update MSKFirehoseResponse.java
    Expanded the Lombok import, updated java doc for the class and removed the record constructor.

--- aws-lambda-java-events/src/main/java/com/amazonaws/services/lambda/runtime/events/MSKFirehoseResponse.java
+++ aws-lambda-java-events/src/main/java/com/amazonaws/services/lambda/runtime/events/MSKFirehoseResponse.java
@@ -3,10 +3,17 @@ package com.amazonaws.services.lambda.runtime.events;
 import java.nio.ByteBuffer;
 import java.util.List;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Response model for Amazon Data Firehose Lambda transformation with MSK as a source.
+ * [+] Amazon Data Firehose Data Transformation - Data Transformation and Status Model - <a href="https://docs.aws.amazon.com/firehose/latest/dev/data-transformation.html#data-transformation-status-model">...</a>
+ * OK : Indicates that processing of this item succeeded.
+ * ProcessingFailed : Indicate that the processing of this item failed.
+ * Dropped : Indicates that this item should be silently dropped
  */
 
 @Data
@@ -17,12 +24,12 @@ import lombok.*;
 public class MSKFirehoseResponse {
 
     public enum Result {
-
+        
         /**
          * Indicates that processing of this item succeeded.
          */
         Ok,
-
+        
         /**
          * Indicate that the processing of this item failed
          */
@@ -36,19 +43,14 @@ public class MSKFirehoseResponse {
     public List<Record> records;
 
     @Data
-    @Builder(setterPrefix = "with")
     @NoArgsConstructor
+    @Builder(setterPrefix = "with")
+    @AllArgsConstructor
 
     public static class Record {
         public String recordId;
         public Result result;
         public ByteBuffer kafkaRecordValue;
 
-        public Record(String recordId, Result result, ByteBuffer kafkaRecordValue) {
-            super();
-            this.recordId = recordId;
-            this.result = result;
-            this.kafkaRecordValue = kafkaRecordValue;
-        }
     }
 }

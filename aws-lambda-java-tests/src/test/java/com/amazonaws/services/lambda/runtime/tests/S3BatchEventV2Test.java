aa399f58791eaf0273c3780a7b5f477806a55731
Martin Merfort <395822+mmerfort@users.noreply.github.com>
2024-07-29 13:13:57 +0200

    Add S3BatchEventV2 (#496)
    * Add S3BatchEventV2 class
    
    * Add test for S3BatchEventV2
    
    ---------
    
    Co-authored-by: Martin Merfort <mmerfort@users.noreply.github.com>
    Co-authored-by: Alexander Smirnov <75367056+smirnoal@users.noreply.github.com>

---
+++ aws-lambda-java-tests/src/test/java/com/amazonaws/services/lambda/runtime/tests/S3BatchEventV2Test.java
@@ -0,0 +1,20 @@
+package com.amazonaws.services.lambda.runtime.tests;
+
+import com.amazonaws.services.lambda.runtime.events.S3BatchEventV2;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class S3BatchEventV2Test {
+
+    @Test
+    public void testS3BatchEventV2() {
+        S3BatchEventV2 event = EventLoader.loadS3BatchEventV2("s3_batch_event_v2.json");
+        assertThat(event).isNotNull();
+        assertThat(event.getInvocationId()).isEqualTo("Jr3s8KZqYWRmaiBhc2RmdW9hZHNmZGpmaGFzbGtkaGZzatx7Ruy");
+        assertThat(event.getJob()).isNotNull();
+        assertThat(event.getJob().getUserArguments().get("MyDestinationBucket")).isEqualTo("destination-directory-bucket-name");
+        assertThat(event.getTasks()).hasSize(1);
+        assertThat(event.getTasks().get(0).getS3Key()).isEqualTo("s3objectkey");
+    }
+}

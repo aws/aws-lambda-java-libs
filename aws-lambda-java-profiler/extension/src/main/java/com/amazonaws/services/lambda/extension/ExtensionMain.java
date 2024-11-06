cc78e05f3bd9d7dbc2722c48166a71a77f97d687
Mark Sailes <45629314+msailes@users.noreply.github.com>
2024-11-06 10:26:49 +0000

    feat: profiler

---
+++ aws-lambda-java-profiler/extension/src/main/java/com/amazonaws/services/lambda/extension/ExtensionMain.java
@@ -0,0 +1,133 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+package com.amazonaws.services.lambda.extension;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URI;
+import java.util.UUID;
+
+public class ExtensionMain {
+
+    private static final HttpClient client = HttpClient.newBuilder().build();
+    private static String previousFileSuffix = null;
+    private static boolean coldstart = true;
+    private static final String REQUEST_ID = "requestId";
+    private static final String EVENT_TYPE = "eventType";
+    public static final String HEADER_NAME = "X-FileName";
+
+    private static S3Manager s3Manager;
+
+    public static void main(String[] args) {
+        final String extension = ExtensionClient.registerExtension();
+        Logger.debug("Extension registration complete, extensionID: " + extension);
+        s3Manager = new S3Manager();
+        while (true) {
+            try {
+                String response = ExtensionClient.getNext(extension);
+                if (response != null && !response.isEmpty()) {
+                    final String eventType = extractInfo(EVENT_TYPE, response);
+                    Logger.debug("eventType = " + eventType);
+                    if (eventType != null) {
+                        switch (eventType) {
+                            case "INVOKE":
+                                handleInvoke(response);
+                                break;
+                            case "SHUTDOWN":
+                                handleShutDown();
+                                break;
+                            default:
+                                Logger.error("invalid event type received " + eventType);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                Logger.error("error while processing extension -" + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void handleShutDown() {
+        Logger.debug("handling SHUTDOWN event, flushing the last profile");
+        try {
+            // no need to stop the profiler as it has been stopped by the shutdown hook
+            s3Manager.upload(previousFileSuffix, true);
+        } catch (Exception e) {
+            Logger.error("could not start the profiler");
+            throw e;
+        }
+        System.exit(0);
+    }
+
+    public static void handleInvoke(String payload) {
+        final String requestId = extractInfo(REQUEST_ID, payload);
+        final String randomSuffix = UUID.randomUUID().toString().substring(0,5);
+        Logger.debug("handling INVOKE event, requestID = " + requestId);
+        if (!coldstart) {
+            try {
+                stopProfiler(previousFileSuffix);
+                s3Manager.upload(previousFileSuffix, false);
+                startProfiler();
+            } catch (Exception e) {
+                Logger.error("could not start the profiler");
+                throw e;
+            }
+        }
+        coldstart = false;
+        previousFileSuffix = extractInfo(REQUEST_ID, payload) + "-" + randomSuffix;
+    }
+
+    private static String extractInfo(String info, String jsonString) {
+        String prefix = "\"" + info + "\":\"";
+        String suffix = "\"";
+        
+        int startIndex = jsonString.indexOf(prefix);
+        if (startIndex == -1) {
+            return null; // requestId not found
+        }
+        
+        startIndex += prefix.length();
+        int endIndex = jsonString.indexOf(suffix, startIndex);
+        
+        if (endIndex == -1) {
+            return null; // Malformed JSON
+        }
+        
+        return jsonString.substring(startIndex, endIndex);
+    }
+
+    private static void startProfiler() {
+        try {
+           HttpRequest request = HttpRequest.newBuilder()
+                            .GET()
+                            .uri(URI.create("http://localhost:1234/profiler/start"))
+                            .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                Logger.debug("profiler successfully started");
+            }
+        } catch(Exception e) {
+            Logger.error("could not start the profiler");
+            e.printStackTrace();
+        }
+    }
+
+    private static void stopProfiler(String fileNameSuffix) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                            .GET()
+                            .setHeader(HEADER_NAME, fileNameSuffix)
+                            .uri(URI.create("http://localhost:1234/profiler/stop"))
+                            .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                Logger.debug("profiler successfully stopped");
+            }
+        } catch(Exception e) {
+            Logger.error("could not stop the profiler");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ aws-lambda-java-profiler/extension/src/main/java/com/amazonaws/services/lambda/extension/S3Manager.java
@@ -0,0 +1,67 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+package com.amazonaws.services.lambda.extension;
+
+import java.io.File;
+import java.time.format.DateTimeFormatter;
+import java.time.Instant;
+import java.time.LocalDate;
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+
+public class S3Manager {
+
+    private static final String RESULTS_BUCKET = "PROFILER_RESULTS_BUCKET_NAME";
+    private static final String FUNCTION_NAME = System.getenv().getOrDefault("AWS_LAMBDA_FUNCTION_NAME", "function");
+    private S3Client s3Client;
+    private String bucketName;
+
+    public S3Manager() {
+        final String bucketName = System.getenv(RESULTS_BUCKET);
+        Logger.debug("creating S3Manager with bucketName = " + bucketName);
+        if (null == bucketName || bucketName.isEmpty()) {
+            throw new IllegalArgumentException("please set the bucket name using PROFILER_RESULTS_BUCKET_NAME environment variable");
+        }
+        this.s3Client = S3Client.builder().build();
+        this.bucketName = bucketName;
+        Logger.debug("S3Manager successfully created");
+    }
+
+    public void upload(String fileName, boolean isShutDownEvent) {
+        try {
+            final String suffix = isShutDownEvent ? "shutdown" : fileName;
+            final String key = buildKey(FUNCTION_NAME, fileName);
+            Logger.debug("uploading profile to key = " + key);
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+            File file = new File(String.format("/tmp/profiling-data-%s.html", suffix));
+            if (file.exists()) {
+                Logger.debug("file size is " + file.length());
+                RequestBody requestBody = RequestBody.fromFile(file);
+                PutObjectResponse response = s3Client.putObject(putObjectRequest, requestBody);
+                Logger.debug("profile uploaded successfully. ETag: " + response.eTag());
+                if(file.delete()) {
+                    Logger.debug("file deleted");
+                }
+            } else {
+                throw new IllegalArgumentException("could not find the profile to upload");
+            }
+        } catch (Exception e) {
+            Logger.error("could not upload the profile");
+            e.printStackTrace();
+        }
+    }
+
+    private String buildKey(String functionName, String fileName) {
+        final LocalDate currentDate = LocalDate.now();
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        final String formattedDate = currentDate.format(formatter);
+        return String.format("%s/%s/%s", formattedDate, functionName, fileName);
+    }
+
+}

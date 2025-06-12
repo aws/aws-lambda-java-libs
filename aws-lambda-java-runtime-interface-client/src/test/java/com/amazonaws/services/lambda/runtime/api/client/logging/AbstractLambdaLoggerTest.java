a66ed4bdd0a461f486253c1f9adb9f20f5783f86
Mohammed Ehab Elsaeed <33024315+M-Elsaeed@users.noreply.github.com>
2025-06-12 14:17:36 +0100

    Support Multiconcurrent Logging (#86)
    * Support Multiconcurrent Logging
    ---------
    
    Co-authored-by: Mohammed Ehab <moehabe@amazon.com>

--- aws-lambda-java-runtime-interface-client/src/test/java/com/amazonaws/services/lambda/runtime/api/client/logging/AbstractLambdaLoggerTest.java
+++ aws-lambda-java-runtime-interface-client/src/test/java/com/amazonaws/services/lambda/runtime/api/client/logging/AbstractLambdaLoggerTest.java
@@ -5,10 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.amazonaws.services.lambda.runtime.logging.LogFormat;
 import org.junit.jupiter.api.Test;
 
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import com.amazonaws.lambda.thirdparty.org.json.JSONObject;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.api.client.api.LambdaContext;
 import com.amazonaws.services.lambda.runtime.logging.LogLevel;
 
 
@@ -20,12 +25,12 @@ public class AbstractLambdaLoggerTest {
         }
 
         @Override
-        public void log(byte[] message) {
+        public synchronized void log(byte[] message) {
             messages.add(message);
         }
 
         @Override
-        public void log(LogLevel logLevel, LogFormat logFormat, byte[] message) {
+        public synchronized void log(LogLevel logLevel, LogFormat logFormat, byte[] message) {
             messages.add(message);
         }
 
@@ -60,6 +65,40 @@ public class AbstractLambdaLoggerTest {
 
         assertEquals("null", new String(sink.getMessages().get(0)));
         assertEquals("null", new String(sink.getMessages().get(1)));
+    }
+
+    // Makes Sure Logging Contexts are thread local.
+    @Test
+    public void testMultiConcurrentLoggingWithoutLogLevelInJSON() {
+        TestSink sink = new TestSink();
+        LambdaContextLogger logger = new LambdaContextLogger(sink, LogLevel.INFO, LogFormat.JSON);
+
+        String someMessagePrefix = "Some Message from ";
+        String reqIDPrefix = "Thread ID as request# ";
+
+        final int nThreads = 5;
+        ExecutorService es = Executors.newFixedThreadPool(nThreads);
+        for (int i = 0; i < nThreads; i++) {
+            es.submit(() -> logger.setLambdaContext(new LambdaContext(Integer.MAX_VALUE, Long.MAX_VALUE, reqIDPrefix + Thread.currentThread().getName(), "", "", "", null, "", "", "", null)));
+        }
+
+        final int nMessages = 100_000;
+        for (int i = 0; i < nMessages; i++) {
+            es.submit(() -> logger.log(someMessagePrefix + Thread.currentThread().getName()));
+        }
+
+        es.shutdown();
+        while (!es.isTerminated()) {
+            ;
+        }
+
+        assertEquals(nMessages, sink.getMessages().size());  
+        for (byte[] message : sink.getMessages()) {
+            JSONObject parsedLog = new JSONObject(new String(message, StandardCharsets.UTF_8));
+            String parsedMessage = parsedLog.getString("message");
+            String parsedReqID = parsedLog.getString("AWSRequestId");
+            assertEquals(parsedMessage.substring(someMessagePrefix.length()), parsedReqID.substring(reqIDPrefix.length()));
+        }
     }
 
     @Test

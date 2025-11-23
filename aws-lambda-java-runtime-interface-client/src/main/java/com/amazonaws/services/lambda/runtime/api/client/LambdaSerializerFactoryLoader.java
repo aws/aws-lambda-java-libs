5e74092da5874cf03fc5c5c4ac24b35fcb37dcd4
Davide Melfi <dmelfi@amazon.com>
2025-11-23 10:01:16 +0000

    chore: creating the loader

---
+++ aws-lambda-java-runtime-interface-client/src/main/java/com/amazonaws/services/lambda/runtime/api/client/LambdaSerializerFactoryLoader.java
@@ -0,0 +1,45 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.amazonaws.services.lambda.runtime.api.client;
+
+import com.amazonaws.services.lambda.runtime.serialization.interfaces.LambdaSerializerFactory;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+public class LambdaSerializerFactoryLoader {
+    private static LambdaSerializerFactory factory;
+    private static boolean initialized = false;
+
+    public static LambdaSerializerFactory load() {
+        if (!initialized) {
+            factory = loadFactory();
+            initialized = true;
+        }
+
+        return factory;
+    }
+
+    private static LambdaSerializerFactory loadFactory() {
+        Iterator<LambdaSerializerFactory> serializers = ServiceLoader
+                .load(LambdaSerializerFactory.class, AWSLambda.customerClassLoader)
+                .iterator();
+
+        if (!serializers.hasNext()) {
+            return null;
+        }
+
+        LambdaSerializerFactory factory = serializers.next();
+
+        if (serializers.hasNext()) {
+            throw new TooManyServiceProvidersFoundException(
+                    "Too many serializers provided inside the META-INF/services folder, only one is allowed"
+            );
+        }
+
+        return factory;
+    }
+}

2c29a691d12473efe658d2cc59bed1d46b606ef7
Davide Melfi <dmelfi@amazon.com>
2026-03-22 12:00:12 +0000

    revert: move test fixes and JDK matrix to improve-action-speed
    ClasspathLoaderTest (JDK 25 split), UnsafeUtilTest (Unsafe.getObjectVolatile
    rewrite), and the JDK version matrix in the RIC PR workflow are moved to the
    dmelfi/improve-action-speed branch. This branch keeps only CI/workflow
    improvements and the JUnit 5.14.3 upgrade.

--- aws-lambda-java-runtime-interface-client/src/test/java/com/amazonaws/services/lambda/runtime/api/client/util/UnsafeUtilTest.java
+++ aws-lambda-java-runtime-interface-client/src/test/java/com/amazonaws/services/lambda/runtime/api/client/util/UnsafeUtilTest.java
@@ -6,8 +6,6 @@ SPDX-License-Identifier: Apache-2.0
 package com.amazonaws.services.lambda.runtime.api.client.util;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledForJreRange;
-import org.junit.jupiter.api.condition.JRE;
 import java.lang.reflect.Field;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,7 +19,7 @@ public class UnsafeUtilTest {
     @Test
     void testThrowException() {
         Exception testException = new Exception("Test exception");
-
+        
         try {
             UnsafeUtil.throwException(testException);
             fail("Should have thrown an exception");
@@ -31,29 +29,22 @@ public class UnsafeUtilTest {
         }
     }
 
-    // IllegalAccessLogger only exists on JDK 9-16; skipped on JDK 8 (no module
-    // system) and JDK 17+ (class removed).
     @Test
-    @EnabledForJreRange(min = JRE.JAVA_9, max = JRE.JAVA_16)
-    void testDisableIllegalAccessWarning() throws Exception {
-        // We disable the warning log for "jdk.internal.module.IllegalAccessLogger"
-        UnsafeUtil.disableIllegalAccessWarning();
-
-        Class<?> illegalAccessLoggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
-        Field loggerField = illegalAccessLoggerClass.getDeclaredField("logger");
-
-        // Now we are getting it back with getObjectVolatile and verify that the logger
-        // is null. We are not using default reflection because that will throw because
-        // that field is private, defeating the point of the test.
-        Object loggerValue = UnsafeUtil.TheUnsafe.getObjectVolatile(
-                illegalAccessLoggerClass,
-                UnsafeUtil.TheUnsafe.staticFieldOffset(loggerField));
-        assertNull(loggerValue);
-    }
-
-    @Test
-    void testDisableIllegalAccessWarningDoesNotThrow() {
+    void testDisableIllegalAccessWarning() {
         assertDoesNotThrow(() -> UnsafeUtil.disableIllegalAccessWarning());
+        try {
+            Class<?> illegalAccessLoggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field loggerField = illegalAccessLoggerClass.getDeclaredField("logger");
+            loggerField.setAccessible(true);
+            Object loggerValue = loggerField.get(null);
+            assertNull(loggerValue);
+        } catch (ClassNotFoundException e) {
+            assertTrue(true);
+        } catch (NoSuchFieldException e) {
+            assertTrue(true);
+        } catch (Exception e) {
+            fail("Unexpected exception: " + e.getMessage());
+        }
     }
 
     @Test
